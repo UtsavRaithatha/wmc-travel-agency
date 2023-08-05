1a2bc568227c52af568f07e006f5a47483300106
UtsavRaithatha <utsav.r@ahduni.edu.in>
2023-08-05 20:21:46 +0530

    Add FAQ, Contact, About page

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -9,7 +9,7 @@ function About() {
                 <div className="row justify-content-center">
                     <div className="col-md-6">
                         <p>
-                        Mystical Voyages - Where Imagination Takes Flight. Unleash the magic within as we transport you to fantastical realms. Experience the wonders of enchanted destinations with our expertly crafted journeys. Let your dreams soar with Mystical Voyages - Your gateway to extraordinary adventures.
+                            Mystical Voyages - Where Imagination Takes Flight. Unleash the magic within as we transport you to fantastical realms. Experience the wonders of enchanted destinations with our expertly crafted journeys. Let your dreams soar with Mystical Voyages - Your gateway to extraordinary adventures.
                         </p>
                     </div>
                 </div>
