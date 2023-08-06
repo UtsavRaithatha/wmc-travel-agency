8ffa3b42c3a194109313fc3d61b27c46b0d092d0
UtsavRaithatha <utsav.r@ahduni.edu.in>
2023-08-06 19:36:57 +0530

    add headers

--- server/app.js
+++ server/app.js
@@ -18,6 +18,14 @@ const app = express();
 connection();
 
 // middlewares
+
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", process.env.CLIENT_URL);
+    res.header("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE");
+    res.header("Access-Control-Allow-Headers", "Content-Type, X-Auth-Token, Origin, Authorization");
+    next();
+});
+
 app.use(express.json());
 app.use(cors({
     origin: process.env.CLIENT_URL,
